6bdf8953e502d445e76025cdabc78b74032efedd
gowdas1997 <154775960+gowdas1997@users.noreply.github.com>
2024-11-03 10:52:50 +0530

    Update Dog.java

--- simple-springboot-app/src/main/java/com/springboot/service/Dog.java
+++ simple-springboot-app/src/main/java/com/springboot/service/Dog.java
@@ -1,12 +1,14 @@
 package com.springboot.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+@Service
+@Qualifier("dog")
 public class Dog implements Animal {
 
-	@Override
-	public String characteristics() {
-		// TODO Auto-generated method stub
-		return "Bark";
-	}
+    @Override
+    public String characteristics() {
+        return "Bark";
+    }
 }
