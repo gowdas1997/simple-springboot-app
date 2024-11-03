42d7e9b48570c6e2b6df8feaef9d9a40c6c31299
gowdas1997 <154775960+gowdas1997@users.noreply.github.com>
2024-11-03 10:55:13 +0530

    Update AutowiredController.java

--- simple-springboot-app/src/main/java/com/springboot/controller/AutowiredController.java
+++ simple-springboot-app/src/main/java/com/springboot/controller/AutowiredController.java
@@ -11,41 +11,17 @@ import com.springboot.service.Animal;
 @RestController
 @RequestMapping("/autowire")
 public class AutowiredController {
-	
-	private Animal animal;
 
-	//Setter Injection
-	@Autowired
-	public void setAnimal(@Qualifier("dog") Animal animal) {
-		this.animal = animal;
-	}
+    private Animal animal;
 
-	//Construction Injection
-	/*@Autowired
-	AutowiredController(@Qualifier("dog") Animal animal) {
-		this.animal = animal;
-	}*/
+    // Active Setter Injection with @Qualifier
+    @Autowired
+    public void setAnimal(@Qualifier("dog") Animal animal) {
+        this.animal = animal;
+    }
 
-	//Setter Injection is non-mandatory injection, server will start and work
-	/*@Autowired(required = false)
-	public void setAnimal(@Qualifier("dog") Animal animal) {
-		this.animal = animal;
-	}*/
-	
-	//Construction Injection is mandatory Injection and this will not work and server will not start
-	/*@Autowired(required = false)
-		AutowiredController(@Qualifier("dog") Animal animal) {
-			this.animal = animal;
-		}*/
-
-	//Using Qualifier
-	/*@Qualifier("cat")
-	@Autowired
-	private Animal animal;*/
-
-	@GetMapping
-	public String fetchDogCharacteristics() {
-		return animal.characteristics();
-	}
-
+    @GetMapping
+    public String fetchDogCharacteristics() {
+        return animal.characteristics();
+    }
 }
