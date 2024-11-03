5a9fa6965bcd22b1bd83f7bf960faf66fc976fed
gowdas1997 <154775960+gowdas1997@users.noreply.github.com>
2024-11-03 11:12:33 +0530

    Create SpringBootAutowireApplication.java

---
+++ simple-springboot-app/src/main/java/com/springboot/SpringBootAutowireApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutowireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutowireApplication.class, args);
+    }
+}
