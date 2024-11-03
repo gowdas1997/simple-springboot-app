68205a844d8e253a565d1ece1ce60140eb20d4d7
gowdas1997 <154775960+gowdas1997@users.noreply.github.com>
2024-11-03 11:30:28 +0530

    Update StudentApplicationTests.java

--- simple-springboot-app/src/test/java/com/springboot/StudentApplicationTests.java
+++ simple-springboot-app/src/test/java/com/springboot/StudentApplicationTests.java
@@ -1,11 +1,8 @@
 package com.springboot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test; // Changed to JUnit 5 import
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentApplicationTests {
 
